bf580d78beeb999c53efe37fbc0731d8957f41c6
jeefersan <ksng.ge@gmail.com>
2020-10-24 02:28:12 +0200

    version 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-function capitalize(input) {
-  let str = input.toLowerCase();
-  let firstLetter = str[0].toUpperCase();
-  let subString = str.slice(1);
-  return firstLetter + subString;
-}
-
 let _playerScore = 0;
 let _computerScore = 0;
-game();
+let gameCount = 0;
+
+setupGameButtons();
 
 function computerPlay() {
   let random = Math.floor(Math.random() * 3);
@@ -20,47 +15,100 @@ function computerPlay() {
 function game() {
   let playRounds = 5;
 
-  const playerScore = document.querySelector("player-score");
-  const computerScore = document.querySelector("computer-score");
+  alert("The battle has started!");
+  setScore();
 
-  for (let i = 0; i < playRounds; i++) {
-    playerScore.textContent = "SCORE: " + _playerScore;
-    computerScore = "SCORE: " + _computerScore;
+  setupChoiceButtons();
 
-    let playerInput = prompt("Enter your choice (rock, paper or scissors");
-    let playerChoice = capitalize(playerInput);
-    playRound(playerChoice, computerPlay());
-  }
+  displayRoundInfo();
 
-  alert(
-    `Results of this game:\nPlayer has won ${playerScore} rounds and computer has won ${computerScore}`
-  );
+  // alert(
+  //   `Results of this game:\nPlayer has won ${playerScore} rounds and computer has won ${computerScore}`
+  // );
 
-  if (playerScore == computerScore) {
-    alert("The final result is a Tie!");
+  // if (_playerScore == _computerScore) {
+  //   alert("The final result is a Tie!");
+  // } else {
+  //   if (_playerScore > _computerScore) {
+  //     alert("Congratulations, you are the winner!");
+  //   } else {
+  //     alert("Too bad, computer wins!");
+  //   }
+  // }
+}
+
+function setupChoiceButtons() {
+  const buttons = Array.from(document.querySelectorAll(".btn-choice"));
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      playRound(btn.id);
+    });
+  });
+}
+
+function setupGameButtons() {
+  const startBtn = document.getElementById("btn-start");
+  startBtn.addEventListener("click", game);
+  const resetBtn = document.getElementById("btn-reset");
+  resetBtn.addEventListener("click", resetGame);
+}
+
+function setScore(didPlayerWin) {
+  // if (didPlayerWin) _playerScore++;
+  // else _computerScore++;
+
+  const playerScore = document.getElementById("player-score");
+  const computerScore = document.getElementById("computer-score");
+
+  playerScore.textContent = "SCORE: " + _playerScore;
+  computerScore.textContent = "SCORE: " + _computerScore;
+}
+
+function displayResult(gameResult) {
+  const result = document.getElementById("result");
+  result.textContent = gameResult;
+}
+
+function playRound(playerSelection) {
+  const computerSelection = computerPlay();
+
+  let result;
+  `You Win! ${playerSelection} beats ${computerSelection}`;
+  if (playerSelection == computerSelection) {
+    result = `You have both selected ${playerSelection}, it's a Tie!`;
+    console.log(result);
   } else {
-    if (playerScore > computerScore) {
-      alert("Congratulations, you are the winner!");
+    if (isPlayerWinner(playerSelection, computerSelection)) {
+      result = `You Win! ${playerSelection} beats ${computerSelection}`;
+      console.log(result);
+      _playerScore++;
     } else {
-      alert("Too bad, computer wins!");
+      result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      console.log(result);
+      _computerScore++;
     }
+  }
+  setScore();
+
+  displayResult(result);
+  gameCount++;
+}
+
+function displayRoundInfo() {
+  const roundInfo = document.getElementById("round-info");
+  if (gameCount == 5) {
+    roundInfo.textContent = "FINAL ROUND!";
+  } else {
+    roundInfo.textContent = "ROUND: " + gameCount;
   }
 }
 
-function getPlayerChoice() {}
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) {
-    console.log(`You have both selected ${playerSelection}, it's a Tie!`);
-  } else {
-    if (isPlayerWinner(playerSelection, computerSelection)) {
-      console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-      playerScore++;
-    } else {
-      console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-      computerScore++;
-    }
-  }
+function resetGame() {
+  _playerScore = 0;
+  _computerScore = 0;
+  gameCount = 0;
+  setScore();
 }
 
 function isPlayerWinner(a, b) {
